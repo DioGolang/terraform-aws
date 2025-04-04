2a967947d79c8349886515a7cbcc7d1b95f6dd09
dvasconcelos <di0g0vasconcelos@hotmail.com>
2025-04-04 14:52:29 -0300

    Add .pre-commit-config.yaml

--- provider.tf
+++ provider.tf
@@ -1,9 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.94.0"
     }
+  }
+  backend "s3" {
+    bucket = "eksbucket-terraform-state"
+    key    = "terraform/dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
